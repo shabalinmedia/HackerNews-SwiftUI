66b3896495dfe31ae35d2c2a19b917df1a45b98a
Alex Shabalin <shabalin.media@gmail.com>
2022-09-09 09:51:36 +0600

    First commit.

---
+++ Hacker News/Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Alexander Shabalin on 9/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
